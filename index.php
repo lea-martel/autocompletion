9ce863fe7154625a3c8b6c96a384164bb590cfdf
lea-martel <lea.martel@laplateforme.io>
2020-12-29 21:42:14 +0100

    js

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/css/fa.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -14,11 +13,72 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles/css/styles.css">
-    <title>Trouve ton pays</title>
+    <title>Voyage</title>
 </head>
 <body>
 <?php include 'src/Base.php'; ?>
 <main>
+    <form class="search-content">
+        <div class="autocomplete">
+            <input class="search-input" type="text" placeholder="Recherche ta destination">
+        </div>
+        <input type="submit">
+    </form>
 </main>
 </body>
 </html>
+<script>
+    function auto(value) {
+        let html = '<div class="search-content">';
+        html += '<div class="search-input">';
+        html += '</div>'
+        html += '</div>'
+        return html + '</div>'
+    }
+
+    $('.search-input').on('input', function (e) {
+        e.preventDefault();
+        let value = $(this).val();
+        let offset = $(this).offset();
+        $.ajax({
+            url: 'search.php',
+            method: 'POST',
+            dataType: 'json',
+            data: {
+                q: value
+            },
+            success: (data) => {
+                let html = '';
+                data.forEach((value) => {
+                    html += auto(value);
+                })
+                if ($('.search-block').length === 0) {
+                    let divSearch = $('<div></div>');
+                    divSearch.addClass('search-block');
+                    divSearch.html(html);
+                    $('body').append(divSearch);
+                    divSearch.css('display', 'none')
+                    divSearch.stop(true, true).slideDown();
+
+                    divSearch.css('top', offset.top + 40).css('left', 10)
+                } else {
+                    let divSearch = $('.search-block');
+                    divSearch.stop(true, true).slideDown();
+                    divSearch.html(html);
+                }
+            },
+            error: (error) => {
+                console.log(error.responseText)
+            }
+        });
+    })
+    $('body').click(function (e) {
+        let div = $(this).find('.search-input');
+        let div2 = $(this).find('.search-block');
+        if (div2.length) {
+            if ((!$(e.target).is(div) && !$.contains(div[0], e.target)) && (!$(e.target).is(div2) && !$.contains(div2[0], e.target))) {
+                div2.stop(true, true).slideUp();
+            }
+        }
+    })
+</script>
